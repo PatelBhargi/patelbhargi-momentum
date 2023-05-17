64b45376ce43e3039eb9c321cd94fe82423365da
PatelBhargi <133199263+PatelBhargi@users.noreply.github.com>
2023-05-17 20:49:31 +0530

    Add files via upload

---
+++ EVENOROD.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+      int n;
+      clrscr();
+      printf("enter n");
+      scanf("%d",&n);
+      (n%2==0)?printf("even"):printf("odd");
+      getch();
+}
