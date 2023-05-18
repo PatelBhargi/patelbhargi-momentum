46ead63546ac0e51e3179567b4369bf88b94f039
PatelBhargi <133199263+PatelBhargi@users.noreply.github.com>
2023-05-18 19:58:44 +0530

    Add files via upload

---
+++ MAXIMUM.C
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+  int a,b,c,d,max;
+  printf("enter 4 numbers:");
+  scanf("%d%d%d%d",&a,&b,&c,&d);
+  if(a>b)
+  {
+    if(a>c)
+    {
+     if(a>d)
+       max=a;
+      else
+	max=d;
+  }
+  else
+  {
+   if(c>d)
+    max=c;
+    else
+    max=d;
+    }
+   }
+   else
+   {
+    if(b>c)
+    {
+      if(b>d)
+      max=b;
+      else
+       max=d;
+     }
+     else
+     {
+	 if(c>d)
+	 max=c;
+	 else
+	 max=d;
+       }
+     }
+     printf("the maximum number is %d",max);
+     getch();
+ }
+
+
+
+
+
+
+
+
